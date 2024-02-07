f2aae182672b8a6cf6d0c71ea34d04b2b9e00e52
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-02-07 14:47:42 +0100

    Configure URL for staging website

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -32,10 +32,11 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.theoplayer.com',
+  url: 'https://theoplayer.prudentgiraffe.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
+  // trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
