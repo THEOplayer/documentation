7a7f61cab56e4360caaf43eafd2f211cf4967195
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-02-07 16:55:24 +0100

    Add trailing slashes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -36,7 +36,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs/',
-  // trailingSlash: false,
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
