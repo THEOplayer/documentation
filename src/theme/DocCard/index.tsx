086c99c9268db4863fac25e46681fbdf8439f66e
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-03-04 15:05:11 +0100

    Run Prettier

--- src/theme/DocCard/index.tsx
+++ src/theme/DocCard/index.tsx
@@ -1,43 +1,36 @@
 import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
-import {
-  findFirstSidebarItemLink,
-  useDocById,
-} from '@docusaurus/theme-common/internal';
+import { findFirstSidebarItemLink, useDocById } from '@docusaurus/theme-common/internal';
 import isInternalUrl from '@docusaurus/isInternalUrl';
-import {translate} from '@docusaurus/Translate';
+import { translate } from '@docusaurus/Translate';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
-function CardContainer({href, children}) {
+
+function CardContainer({ href, children }) {
   return (
-    <Link
-      href={href}
-      className={clsx('card padding--lg', styles.cardContainer)}>
+    <Link href={href} className={clsx('card padding--lg', styles.cardContainer)}>
       {children}
     </Link>
   );
 }
-function CardLayout({href, icon, title, description}) {
+
+function CardLayout({ href, icon, title, description }) {
   return (
     <CardContainer href={href}>
-      <Heading
-        as="h2"
-        className={clsx('text--truncate', styles.cardTitle)}
-        title={title}>
+      <Heading as="h2" className={clsx('text--truncate', styles.cardTitle)} title={title}>
         {icon} {title}
       </Heading>
       {description && (
-        <p
-          className={clsx('text--truncate', styles.cardDescription)}
-          title={description}>
+        <p className={clsx('text--truncate', styles.cardDescription)} title={description}>
           {description}
         </p>
       )}
     </CardContainer>
   );
 }
-function CardCategory({item}) {
+
+function CardCategory({ item }) {
   const href = findFirstSidebarItemLink(item);
   // Unexpected: categories that don't have a link have been filtered upfront
   if (!href) {
@@ -54,28 +47,22 @@ function CardCategory({item}) {
           {
             message: '{count} items',
             id: 'theme.docs.DocCard.categoryDescription',
-            description:
-              'The default description for a category card in the generated index about how many items this category includes',
+            description: 'The default description for a category card in the generated index about how many items this category includes',
           },
-          {count: item.items.length},
+          { count: item.items.length }
         )
       }
     />
   );
 }
-function CardLink({item}) {
+
+function CardLink({ item }) {
   const icon = isInternalUrl(item.href) ? '📄️' : '🔗';
   const doc = useDocById(item.docId ?? undefined);
-  return (
-    <CardLayout
-      href={item.href}
-      icon={icon}
-      title={item.label}
-      description={item.description ?? doc?.description}
-    />
-  );
+  return <CardLayout href={item.href} icon={icon} title={item.label} description={item.description ?? doc?.description} />;
 }
-export default function DocCard({item}) {
+
+export default function DocCard({ item }) {
   switch (item.type) {
     case 'link':
       return <CardLink item={item} />;
