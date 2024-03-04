7d1902a2dfee3ea1d3fff752f41c5e01c933d116
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-03-04 16:10:13 +0100

    Run Prettier

--- src/theme/DocCardList/index.tsx
+++ src/theme/DocCardList/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import clsx from 'clsx';
-import {
-  useCurrentSidebarCategory,
-  filterDocCardListItems,
-} from '@docusaurus/theme-common';
+import { useCurrentSidebarCategory, filterDocCardListItems } from '@docusaurus/theme-common';
 import DocCard from '@theme/DocCard';
-function DocCardListForCurrentSidebarCategory({className}) {
+
+function DocCardListForCurrentSidebarCategory({ className }) {
   const category = useCurrentSidebarCategory();
   return <DocCardList items={category.items} className={className} />;
 }
+
 export default function DocCardList(props) {
-  const {items, className} = props;
+  const { items, className } = props;
   if (!items) {
     return <DocCardListForCurrentSidebarCategory {...props} />;
   }
