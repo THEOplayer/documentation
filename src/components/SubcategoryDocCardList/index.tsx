2ca659999905bcf462ace631ac98ee3f354ffbe6
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-03-08 14:29:28 +0100

    Add missing keys in <SubcategoryDocCardList>

--- src/components/SubcategoryDocCardList/index.tsx
+++ src/components/SubcategoryDocCardList/index.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import DocCardList, { type Props as DocCardListProps } from '@theme/DocCardList';
 import { useCurrentSidebarCategory } from '@docusaurus/theme-common';
 import { PropSidebarItem, PropSidebarItemCategory } from '@docusaurus/plugin-content-docs';
@@ -15,9 +16,9 @@ export default function SubcategoryDocCardList({ headingLevel, ...props }: Props
   const category = useCurrentSidebarCategory();
   const subcategories = category.items.filter(isCategory);
   return subcategories.map((subcategory) => (
-    <>
+    <Fragment key={subcategory.label}>
       <Heading as={headingLevel ?? 'h2'}>{subcategory.label}</Heading>
       <DocCardList items={subcategory.items} {...props} />
-    </>
+    </Fragment>
   ));
 }
