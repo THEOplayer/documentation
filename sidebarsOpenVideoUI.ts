9c987e96f2248fb3020fe26d78d31e8944fd971c
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-03-12 18:28:15 +0100

    Add framework logos

--- sidebarsOpenVideoUI.ts
+++ sidebarsOpenVideoUI.ts
@@ -52,7 +52,7 @@ const sidebars: SidebarsConfig = {
       label: 'React',
       description: 'Build a stunning player UI with React components.',
       customProps: {
-        icon: '⚛️',
+        icon: 'react',
       },
       link: {
         type: 'generated-index',
--- src/theme/DocCard/index.tsx
+++ src/theme/DocCard/index.tsx
@@ -8,9 +8,16 @@ import type { Props } from '@theme/DocCard';
 import type { PropSidebarItemCategory, PropSidebarItemLink } from '@docusaurus/plugin-content-docs';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import AmpIcon from '@site/static/img/amp.svg';
 import AndroidIcon from '@site/static/img/android.svg';
+import AngularIcon from '@site/static/img/angular.svg';
 import AppleIcon from '@site/static/img/apple.svg';
 import ChromecastIcon from '@site/static/img/chromecast.svg';
+import FlutterIcon from '@site/static/img/flutter.svg';
+import NuxtjsIcon from '@site/static/img/nuxtjs.svg';
+import ReactIcon from '@site/static/img/react.svg';
+import VuejsIcon from '@site/static/img/vuejs.svg';
+import WordPressIcon from '@site/static/img/wordpress.svg';
 
 function CardContainer({ href, children }) {
   return (
@@ -46,12 +53,26 @@ function CardIcon({ item, defaultIcon }: CardIconProps): JSX.Element | string | 
     return defaultIcon;
   }
   switch (icon) {
+    case 'amp':
+      return <AmpIcon className={clsx(styles.cardIcon)} />;
     case 'android':
       return <AndroidIcon className={clsx(styles.cardIcon)} />;
+    case 'angular':
+      return <AngularIcon className={clsx(styles.cardIcon)} />;
     case 'apple':
       return <AppleIcon className={clsx(styles.cardIcon)} />;
     case 'chromecast':
       return <ChromecastIcon className={clsx(styles.cardIcon)} />;
+    case 'flutter':
+      return <FlutterIcon className={clsx(styles.cardIcon)} />;
+    case 'nuxtjs':
+      return <NuxtjsIcon className={clsx(styles.cardIcon)} />;
+    case 'react':
+      return <ReactIcon className={clsx(styles.cardIcon)} />;
+    case 'vuejs':
+      return <VuejsIcon className={clsx(styles.cardIcon)} />;
+    case 'wordpress':
+      return <WordPressIcon className={clsx(styles.cardIcon)} />;
     default:
       return icon;
   }
