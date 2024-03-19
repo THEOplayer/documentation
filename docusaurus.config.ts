6867f224a49fa932434ecf4647b0ede72546a82d
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-03-19 14:01:54 +0100

    Update production URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,7 +43,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://theoplayer.prudentgiraffe.com/',
+  url: 'https://www.theoplayer.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: process.env.DOCUSAURUS_BASE_URL || '/docs/',
