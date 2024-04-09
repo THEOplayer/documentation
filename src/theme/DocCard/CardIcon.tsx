08612c088720c4369e967b43cb9cb3d1f74aa59f
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-04-09 15:49:38 +0200

    Add logos and descriptions

--- src/theme/DocCard/CardIcon.tsx
+++ src/theme/DocCard/CardIcon.tsx
@@ -1,15 +1,19 @@
 import React, { JSX } from 'react';
 import clsx from 'clsx';
+import ThemedImage from '@theme/ThemedImage';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import AmpIcon from '@site/static/img/amp.svg';
 import AndroidIcon from '@site/static/img/android.svg';
 import AngularIcon from '@site/static/img/angular.svg';
 import AppleIcon from '@site/static/img/apple.svg';
 import ChromecastIcon from '@site/static/img/chromecast.svg';
 import FlutterIcon from '@site/static/img/flutter.svg';
+import NielsenIcon from '@site/static/img/nielsen.svg';
 import NuxtjsIcon from '@site/static/img/nuxtjs.svg';
 import ReactIcon from '@site/static/img/react.svg';
 import VuejsIcon from '@site/static/img/vuejs.svg';
 import WordPressIcon from '@site/static/img/wordpress.svg';
+import YospaceIconUrl from '@site/static/img/yospace.png';
 import styles from './styles.module.css';
 
 interface CardIconProps {
@@ -32,8 +36,20 @@ export default function CardIcon({ icon, defaultIcon }: CardIconProps): JSX.Elem
       return <AppleIcon className={clsx(styles.cardIcon)} />;
     case 'chromecast':
       return <ChromecastIcon className={clsx(styles.cardIcon)} />;
+    case 'conviva':
+      return (
+        <ThemedImage
+          className={clsx(styles.cardIcon)}
+          sources={{
+            light: useBaseUrl('/img/conviva.svg'),
+            dark: useBaseUrl('/img/conviva_dark.svg'),
+          }}
+        />
+      );
     case 'flutter':
       return <FlutterIcon className={clsx(styles.cardIcon)} />;
+    case 'nielsen':
+      return <NielsenIcon className={clsx(styles.cardIcon)} />;
     case 'nuxtjs':
       return <NuxtjsIcon className={clsx(styles.cardIcon)} />;
     case 'react':
@@ -42,6 +58,8 @@ export default function CardIcon({ icon, defaultIcon }: CardIconProps): JSX.Elem
       return <VuejsIcon className={clsx(styles.cardIcon)} />;
     case 'wordpress':
       return <WordPressIcon className={clsx(styles.cardIcon)} />;
+    case 'yospace':
+      return <img src={YospaceIconUrl} className={clsx(styles.cardIcon)} />;
     default:
       return icon;
   }
