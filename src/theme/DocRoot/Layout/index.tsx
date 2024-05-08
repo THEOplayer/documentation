cf651d96e1aa28fdd1c154bd1e691c72a6a8572e
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-05-08 16:59:53 +0200

    Override sidebar for shared docs

--- src/theme/DocRoot/Layout/index.tsx
+++ src/theme/DocRoot/Layout/index.tsx
@@ -1,21 +1,44 @@
-import React, { JSX, useEffect } from 'react';
+import React, { JSX, useEffect, useMemo } from 'react';
 import Layout from '@theme-original/DocRoot/Layout';
 import type LayoutType from '@theme/DocRoot/Layout';
 import type { WrapperProps } from '@docusaurus/types';
-import { useDocsSidebar } from '@docusaurus/theme-common/internal';
+import { DocsSidebarProvider, useDocsSidebar, useDocsVersion } from '@docusaurus/theme-common/internal';
 import { isPlatformName, useLastPlatform } from '@site/src/contexts/lastPlatform';
+import { useActiveDocContext, useActivePlugin } from '@docusaurus/plugin-content-docs/client';
 
 type Props = WrapperProps<typeof LayoutType>;
 
 export default function LayoutWrapper(props: Props): JSX.Element {
+  const { pluginId } = useActivePlugin({ failfast: true });
+  const { activeDoc } = useActiveDocContext(pluginId);
+  const docSidebar = useDocsSidebar();
+  const versionMetadata = useDocsVersion();
+  const { lastPlatformName, saveLastPlatform } = useLastPlatform();
+
+  // Override sidebar for certain docs that are shared across platforms
+  const sidebar = useMemo(() => {
+    if (isSharedPlatformDoc(activeDoc.id)) {
+      const sidebarName = lastPlatformName || 'web';
+      return { name: sidebarName, items: versionMetadata.docsSidebars[sidebarName] };
+    } else {
+      return docSidebar;
+    }
+  }, [activeDoc.id, lastPlatformName, versionMetadata]);
+
   // Store current sidebar if it's a platform sidebar
-  const { saveLastPlatform } = useLastPlatform();
-  const sidebar = useDocsSidebar();
   useEffect(() => {
     if (sidebar && isPlatformName(sidebar.name)) {
       saveLastPlatform(sidebar.name);
     }
   }, [saveLastPlatform, sidebar?.name]);
 
-  return <Layout {...props} />;
+  return (
+    <DocsSidebarProvider name={sidebar.name} items={sidebar.items}>
+      <Layout {...props} />
+    </DocsSidebarProvider>
+  );
+}
+
+function isSharedPlatformDoc(docId: string) {
+  return docId === 'changelog' || docId.startsWith('how-to-guides/') || docId.startsWith('knowledge-base/');
 }
