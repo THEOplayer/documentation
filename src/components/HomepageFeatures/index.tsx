99a70b3cf6ea8e5170e9b075c0a8301c97336c27
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-09-03 15:22:53 +0200

    Put products in 2x2 grid

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -89,8 +89,8 @@ const FeatureList: FeatureItem[] = [
 
 function Feature({ title, Image, description, to }: FeatureItem) {
   return (
-    <div className={clsx('col col--4 margin-vert--md')}>
-      <div className={clsx('card padding--lg margin-horiz--sm', styles.featureCard)}>
+    <div className={clsx('col col--6 margin-vert--md')}>
+      <div className={clsx('card padding--md margin-horiz--sm', styles.featureCard)}>
         <div className={clsx('card__header text--center', styles.featureHeading)}>
           <Image className={styles.featureImage} alt={title} />
         </div>
