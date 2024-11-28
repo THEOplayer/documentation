82c0ef3b8d2066ac023e63740cfb58c573637ceb
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-11-28 10:35:55 +0100

    Add eslint-plugin-react-compiler

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -12,6 +13,9 @@ export default [
         ...globals.browser,
         ...globals.node,
       },
+    },
+    plugins: {
+      'react-compiler': reactCompiler,
     },
     settings: {
       react: {
@@ -25,6 +29,7 @@ export default [
   pluginReact.configs.flat['jsx-runtime'],
   {
     rules: {
+      'react-compiler/react-compiler': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-namespace': 'off',
       '@typescript-eslint/no-require-imports': 'off',
