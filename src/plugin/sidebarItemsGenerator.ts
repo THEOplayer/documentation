3f70ec36524d8524d167ae1f857f8549f10fba97
Mattias Buelens <mattias.buelens@theoplayer.com>
2024-12-17 11:18:40 +0100

    Remove logic for additional sidebar items

--- src/plugin/sidebarItemsGenerator.ts
+++ src/plugin/sidebarItemsGenerator.ts
@@ -8,12 +8,7 @@ import type {
 type SidebarItemWithCustomProps = NormalizedSidebarItem & {
   customProps?: {
     exclude?: string[];
-    additionalItems?: AdditionalItem[];
   };
-};
-
-type AdditionalItem = NormalizedSidebarItem & {
-  position?: number;
 };
 
 interface PostProcessArgs extends Omit<SidebarItemsGeneratorArgs, 'item'> {
@@ -38,16 +33,6 @@ function postProcessChildren(parent: SidebarItemWithCustomProps, children: Norma
       const child = children[i];
       if (child.type === 'category' && exclude.has(child.label)) {
         children.splice(i, 1);
-      }
-    }
-  }
-  // Add additional items
-  if (parent.customProps?.additionalItems) {
-    for (const additionalItem of parent.customProps.additionalItems) {
-      if (additionalItem.position !== undefined) {
-        children.splice(additionalItem.position - 1, 0, additionalItem);
-      } else {
-        children.push(additionalItem);
       }
     }
   }
