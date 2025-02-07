d09da06de1c7c05064829233ee79233c75eedcbd
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-02-07 11:40:06 +0100

    Update DocCard

--- src/theme/DocCard/index.tsx
+++ src/theme/DocCard/index.tsx
@@ -2,6 +2,7 @@ import React, { type ReactNode } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import { findFirstSidebarItemLink, useDocById } from '@docusaurus/plugin-content-docs/client';
+import { usePluralForm } from '@docusaurus/theme-common';
 import isInternalUrl from '@docusaurus/isInternalUrl';
 import { translate } from '@docusaurus/Translate';
 import type { Props } from '@theme/DocCard';
@@ -9,6 +10,22 @@ import type { PropSidebarItemCategory, PropSidebarItemLink } from '@docusaurus/p
 import Heading from '@theme/Heading';
 import Icon from '@site/src/components/Icon';
 import styles from './styles.module.css';
+
+function useCategoryItemsPlural() {
+  const { selectMessage } = usePluralForm();
+  return (count: number) =>
+    selectMessage(
+      count,
+      translate(
+        {
+          message: '1 item|{count} items',
+          id: 'theme.docs.DocCard.categoryDescription.plurals',
+          description: 'The default description for a category card in the generated index about how many items this category includes',
+        },
+        { count }
+      )
+    );
+}
 
 function CardContainer({ href, children }: { href: string; children: ReactNode }) {
   return (
@@ -45,28 +62,13 @@ function CardIcon({ item, defaultIcon }: SidebarItemCardIconProps): JSX.Element 
 function CardCategory({ item }: { item: PropSidebarItemCategory }) {
   const icon = <CardIcon item={item} defaultIcon="🗃️" />;
   const href = findFirstSidebarItemLink(item);
+  const categoryItemsPlural = useCategoryItemsPlural();
+
   // Unexpected: categories that don't have a link have been filtered upfront
   if (!href) {
     return null;
   }
-  return (
-    <CardLayout
-      href={href}
-      icon={icon}
-      title={item.label}
-      description={
-        item.description ??
-        translate(
-          {
-            message: '{count} items',
-            id: 'theme.docs.DocCard.categoryDescription',
-            description: 'The default description for a category card in the generated index about how many items this category includes',
-          },
-          { count: item.items.length }
-        )
-      }
-    />
-  );
+  return <CardLayout href={href} icon={icon} title={item.label} description={item.description ?? categoryItemsPlural(item.items.length)} />;
 }
 
 function CardLink({ item }: { item: PropSidebarItemLink }) {
