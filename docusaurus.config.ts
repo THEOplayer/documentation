20ab715f3a1c8e55c8bc4298de81e5309f42051a
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-03-25 17:44:56 +0100

    Use SVG for new logo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -432,7 +432,7 @@ const config: Config = {
       title: null,
       logo: {
         alt: 'THEOdocs',
-        src: 'img/dolby-logo-white.svg',
+        src: 'img/dolby-optiview-white.svg',
       },
       items: [
         {
