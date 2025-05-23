cf5c89f4801ff27d7f7482c88a88368c257a82a2
Mattias Buelens <mattias.buelens@theoplayer.com>
2025-05-23 15:03:00 +0200

    Add main doc in breadcrumbs

--- src/theme/DocBreadcrumbs/index.tsx
+++ src/theme/DocBreadcrumbs/index.tsx
@@ -1,7 +1,9 @@
 import React, { type ReactNode } from 'react';
 import clsx from 'clsx';
 import { ThemeClassNames } from '@docusaurus/theme-common';
-import { useSidebarBreadcrumbs } from '@docusaurus/plugin-content-docs/client';
+import { useActiveDocContext, useActivePlugin, useSidebarBreadcrumbs } from '@docusaurus/plugin-content-docs/client';
+import type { PropSidebarBreadcrumbsItem, PropSidebarItemLink } from '@docusaurus/plugin-content-docs/lib/sidebars/types.js';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import { useHomePageRoute } from '@docusaurus/theme-common/internal';
 import Link from '@docusaurus/Link';
 import { translate } from '@docusaurus/Translate';
@@ -38,8 +40,42 @@ function BreadcrumbsItem({ children, active }: { children: ReactNode; active?: b
   );
 }
 
-export default function DocBreadcrumbs(): ReactNode {
+function getMainDocTitle(docPluginId: string): string | null {
+  switch (docPluginId) {
+    case 'theoplayer':
+      return 'Player';
+    case 'open-video-ui':
+      return 'Open Video UI';
+    case 'theoads':
+      return 'Ads';
+    case 'theolive':
+      return 'Live Streaming';
+    case 'millicast':
+      return 'Real-time Streaming';
+  }
+}
+
+function useSidebarBreadcrumbsWithMainDoc(): PropSidebarBreadcrumbsItem[] | null {
   const breadcrumbs = useSidebarBreadcrumbs();
+  const { pluginId } = useActivePlugin({ failfast: true })!;
+  const { activeVersion, activeDoc } = useActiveDocContext(pluginId);
+  const versionMainDoc = activeVersion.docs.find((doc) => doc.id === activeVersion.mainDocId)!;
+  const mainDocUrl = useBaseUrl(versionMainDoc.path);
+  const mainDocTitle = getMainDocTitle(pluginId);
+  if (!breadcrumbs || !mainDocTitle) {
+    return breadcrumbs;
+  }
+  const mainDocItem: PropSidebarItemLink = {
+    type: 'link',
+    href: activeDoc.id === activeVersion.mainDocId ? undefined : mainDocUrl,
+    label: mainDocTitle,
+    docId: versionMainDoc.id,
+  };
+  return [mainDocItem, ...breadcrumbs];
+}
+
+export default function DocBreadcrumbs(): ReactNode {
+  const breadcrumbs = useSidebarBreadcrumbsWithMainDoc();
   const homePageRoute = useHomePageRoute();
 
   if (!breadcrumbs) {
