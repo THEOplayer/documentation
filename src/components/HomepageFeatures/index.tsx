cac7b8ce9b2d406754e7dffa3dd9dce90c90a29c
Dan Coffey <danielpcoffey@gmail.com>
2026-03-23 14:33:36 -0400

    language updates

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -49,8 +49,7 @@ const FeatureList: FeatureItem[] = [
     description: (
       <>
         <p>
-          Dolby OptiView&apos;s Live-streaming solution enables you to stream in ultra-low delay to any audience size with the best quality of
-          experience, whether it’s for sports betting, iGaming or interactive entertainment.
+          Dolby OptiView's Live-streaming solution enables you to stream with consistency to any audience size with the best quality of experience, whether it's for a large live broadcast event, sports betting, or interactive entertainment.
         </p>
         <p>
           With this solution, choose Real-time for interactive latency or manage latency targets from 1 second and up with Live. Use both with the
