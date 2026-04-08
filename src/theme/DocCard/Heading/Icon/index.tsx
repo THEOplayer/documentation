5910d15523e429ce48809be36a75fdaa5c5835de
Mattias Buelens <mattias.buelens@theoplayer.com>
2026-04-08 13:40:29 +0200

    Support custom icons in DocCard

--- src/theme/DocCard/Heading/Icon/index.tsx
+++ src/theme/DocCard/Heading/Icon/index.tsx
@@ -1,10 +1,20 @@
 import React, { type ReactNode } from 'react';
 import clsx from 'clsx';
+import Icon from '@site/src/components/Icon';
 import { ThemeClassNames } from '@docusaurus/theme-common';
 import type { Props } from '@theme/DocCard/Heading/Icon';
 
 import styles from './styles.module.css';
 
-export default function DocCardHeadingIcon({ icon }: Props): ReactNode {
-  return <span className={clsx(ThemeClassNames.docs.docCard.icon, styles.cardTitleIcon)}>{icon}</span>;
+interface SidebarItemCustomProps {
+  icon?: string;
+}
+
+export default function DocCardHeadingIcon({ item, icon }: Props): ReactNode {
+  const customIcon = (item.customProps as SidebarItemCustomProps)?.icon;
+  if (customIcon) {
+    return <Icon icon={customIcon} defaultIcon="" className={clsx(ThemeClassNames.docs.docCard.icon, styles.cardTitleIcon)} />;
+  } else {
+    return <span className={clsx(ThemeClassNames.docs.docCard.icon, styles.cardTitleIcon)}>{icon}</span>;
+  }
 }
