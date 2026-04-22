157f52fb09985d7dc333abb02bac2ff89b469f46
Mattias Buelens <mattias.buelens@theoplayer.com>
2026-04-22 12:14:51 +0200

    Simplify `<CodeInput>`

--- src/components/CodeInput/index.tsx
+++ src/components/CodeInput/index.tsx
@@ -1,37 +1,20 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import AceEditor, { IAceEditorProps } from 'react-ace';
 import 'ace-builds/src-noconflict/mode-css';
 import 'ace-builds/src-noconflict/theme-monokai';
 import 'ace-builds/src-noconflict/ext-language_tools';
 import styles from './styles.module.css';
 
-export interface CodeInputElement {
-  value: string;
-}
-
 export interface Props extends IAceEditorProps {
   lang: string;
-  defaultValue?: string;
-  onInput?: (event: { target: CodeInputElement }) => void;
 }
 
-export function CodeInput({ lang, defaultValue, onInput, ...props }: Props) {
-  const [value, setValue] = useState(defaultValue ?? '');
-  const onChange = useCallback(
-    (newValue: string) => {
-      setValue(newValue);
-      onInput?.({ target: { value: newValue } });
-    },
-    [onInput]
-  );
+export function CodeInput({ lang, ...props }: Props) {
   return (
     <AceEditor
       className={styles.codeInput}
       mode={lang.toLowerCase()}
       theme="monokai"
-      value={value}
-      defaultValue={defaultValue}
-      onChange={onChange}
       width="100%"
       maxLines={20}
       tabSize={4}
