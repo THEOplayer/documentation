1d4bcc7598537ba1ff7df3d535bdcc2a4290c0bd
Mattias Buelens <mattias.buelens@theoplayer.com>
2026-04-22 12:07:07 +0200

    Replace `code-input` with `react-ace`

--- src/components/CodeInput/index.tsx
+++ src/components/CodeInput/index.tsx
@@ -1,39 +1,49 @@
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
-import React, { type DetailedHTMLProps, type HTMLAttributes, useEffect } from 'react';
-import type * as CodeInputNamespace from '@webcoder49/code-input';
-import 'prismjs/themes/prism-okaidia.min.css';
-import '@webcoder49/code-input/code-input.min.css';
+import React, { useCallback, useState } from 'react';
+import AceEditor, { IAceEditorProps } from 'react-ace';
+import 'ace-builds/src-noconflict/mode-css';
+import 'ace-builds/src-noconflict/theme-monokai';
+import 'ace-builds/src-noconflict/ext-language_tools';
+import styles from './styles.module.css';
 
-let codeInput: typeof import('./CodeInputLoader');
-if (ExecutionEnvironment.canUseDOM) {
-  // <code-input> can only be loaded inside the browser
-  codeInput = require('./CodeInputLoader');
-}
-
-export interface CodeInputElement extends CodeInputNamespace.CodeInput {
+export interface CodeInputElement {
   value: string;
 }
 
-export interface Props extends DetailedHTMLProps<HTMLAttributes<CodeInputElement>, CodeInputElement> {
-  template?: string;
-  value?: string;
+export interface Props extends IAceEditorProps {
+  lang: string;
+  defaultValue?: string;
+  onInput?: (event: { target: CodeInputElement }) => void;
 }
 
-declare module 'react' {
-  namespace JSX {
-    interface IntrinsicElements {
-      'code-input': Props;
-    }
-  }
-}
-
-export function CodeInput({ value, defaultValue, onInput, ...props }: Props) {
-  useEffect(() => {
-    codeInput.setupTemplate();
-  }, []);
+export function CodeInput({ lang, defaultValue, onInput, ...props }: Props) {
+  const [value, setValue] = useState(defaultValue ?? '');
+  const onChange = useCallback(
+    (newValue: string) => {
+      setValue(newValue);
+      onInput?.({ target: { value: newValue } });
+    },
+    [onInput]
+  );
   return (
-    <code-input template="syntax-highlighted" {...props} onInput={onInput}>
-      <textarea data-code-input-fallback value={value} defaultValue={defaultValue} />
-    </code-input>
+    <AceEditor
+      className={styles.codeInput}
+      mode={lang.toLowerCase()}
+      theme="monokai"
+      value={value}
+      defaultValue={defaultValue}
+      onChange={onChange}
+      width="100%"
+      maxLines={20}
+      tabSize={4}
+      highlightActiveLine={true}
+      showGutter={true}
+      showPrintMargin={false}
+      wrapEnabled={true}
+      setOptions={{
+        useWorker: false,
+        showLineNumbers: true,
+      }}
+      {...props}
+    />
   );
 }
